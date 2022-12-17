de37695d676f0dddce82a6f2842fc8d9db8bac6c
Sebik3012Sk <sebik-kucera@hotmail.com>
2022-12-17 14:37:49 +0100

    new grid

--- CSharp-Together-Project/obj/Release/net6.0-windows/MainWindow.g.cs
+++ CSharp-Together-Project/obj/Release/net6.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace CSharp_Together_Project {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CSharp-Together-Project;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CSharp-Together-Project;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
