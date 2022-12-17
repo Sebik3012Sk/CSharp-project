024355ed194ba3cc16408be59d36015681f73c77
Sebik3012Sk <sebik-kucera@hotmail.com>
2022-12-17 14:48:44 +0100

    added branch login form

---
+++ CSharp-Together-Project/MainScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Together_Project
+{
+    internal class MainScreen
+    {
+
+        string input_username;
+        string input_password;
+
+        public MainScreen(string input_username , string input_password)
+        {
+            this.input_username = input_username;
+            this.input_password = input_password;
+        }
+    }
+}
