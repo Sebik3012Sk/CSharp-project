14674de7f2a58c63b099a5fff8600f043f5704f4
Sebik3012Sk <sebik-kucera@hotmail.com>
2022-12-25 19:42:07 +0100

    basic program --complete

---
+++ CSharp-Together-Project/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSharp_Together_Project
+{
+    /// <summary>
+    /// Interakční logika pro MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonNextClick(object sender , RoutedEventArgs e)
+        {
+            MainProC main_window = new MainProC();
+            main_window.goPro();
+        }
+
+    }
+}
---
+++ CSharp-Together-Project/MainProC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CSharp_Together_Project
+{
+    internal class MainProC
+    {
+        
+
+        public MainProC()
+        {
+
+        }
+
+        public void goPro()
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
+
+        public void noGoPro()
+        {
+            MessageBox.Show("I´m sorry", "Login Form App");
+        }
+
+    }
+}
